b02a666f035ba68792f6b12b4eff76cd753982f8
joaobug <joaoenfrance@gmail.com>
2024-08-01 17:30:27 -0300

    Finished required task 1

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct CardView: View {
+    @State var content = ""
+    @State var isFaceUp: Bool = true
+ 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
+        
+        ZStack {
+            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            Group{
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                    
+                Text(content)
+                    .font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }}
 #Preview {
     CardView()
 }
--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis = ["👽", "💀","💩","🍄","🧸","👻","🎃","🐭","🐒","🕷️","💊",]
+    
+    @State private var cardCount: Int = 4
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCounAdjuster
+                        
+        } .padding()
+    }
+    
+    func cardCoundAdjuster(by offset: Int, symbol: String) -> some View{
+        Button(action: {
+            cardCount += offset
+            }
+               , label: {
+            Image(systemName: symbol)
+        }).imageScale(.large)
+            .font(.largeTitle)
+            .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardCounAdjuster: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        } .padding()
+    }
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
-        .padding()
+        .foregroundStyle(.orange)
+    }
+
+    var cardAdder: some View{
+  cardCoundAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    var cardRemover: some View {
+        return cardCoundAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
     }
 }
-
 #Preview {
     ContentView()
 }
